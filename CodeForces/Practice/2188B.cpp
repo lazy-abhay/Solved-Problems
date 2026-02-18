bd9600c27e40e21ed51930f75c02971607c81906
Abhay <abhaykrn02@gmail.com>
2026-02-18 22:52:54 +0530

    1000

---
+++ CodeForces/Practice/2188B.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int32_t main(void)
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    auto abhay = [&](int __t) -> void
+    {
+        int n;
+        cin >> n;
+        string str;
+        cin >> str;
+        int ans = 0, cnt = 0, one = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            if (str[i] == '0')
+                cnt++;
+            else
+            {
+                ans += (cnt - 1 - one + 2) / 3;
+                one = 1;
+                cnt = 0;
+                // cout << ans; 
+            }
+        }
+        ans += (cnt - one + 2) / 3;
+        cout << ans + count(all(str), '1');
+    };
+
+    int T = 1;
+    cin >> T;
+    for (int t = 1; t <= T; ++t)
+    {
+        abhay(t);
+        cout << endl;
+    }
+    return 0;
+}
