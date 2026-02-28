b68ea93c219833f4ec1d3b66b6f7133296727261
Abhay <abhaykrn02@gmail.com>
2026-02-28 10:40:46 +0530

    900

---
+++ CodeForces/CP_31_sheet/900/6.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define int long long
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int32_t main(void)
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    auto abhay = [&](int __t) -> void
+    {
+        int n;
+        cin >> n;
+        for (int i = 1; i < 500000; ++i)
+        {
+            if (n % i != 0)
+            {
+                cout << i - 1;
+                return;
+            }
+        }
+    };
+
+    int T = 1;
+    cin >> T;
+    for (int t = 1; t <= T; ++t)
+    {
+        abhay(t);
+        cout << endl;
+    }
+    return 0;
+}
